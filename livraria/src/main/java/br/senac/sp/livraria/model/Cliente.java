bd0e2522dffba285b4cf2bdd249c75778cc793f5
ErickGX <ericksaopaulino1@hotmail.com>
2025-04-25 11:06:39 -0300

    Adicionando lib lombok

--- livraria/src/main/java/br/senac/sp/livraria/model/Cliente.java
+++ livraria/src/main/java/br/senac/sp/livraria/model/Cliente.java
@@ -4,7 +4,14 @@ import java.util.Calendar;
 
 import br.senac.sp.livraria.enumeration.Escolaridade;
 import br.senac.sp.livraria.enumeration.EstadoCivil;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Cliente {
 	private int id;
 	private String nome;
@@ -15,59 +22,4 @@ public class Cliente {
 	private String endereco;
 	private EstadoCivil estadoCivil;
 	private Escolaridade escolaridade;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public Calendar getNascimento() {
-		return nascimento;
-	}
-	public void setNascimento(Calendar nascimento) {
-		this.nascimento = nascimento;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	public EstadoCivil getEstadoCivil() {
-		return estadoCivil;
-	}
-	public void setEstadoCivil(EstadoCivil estadoCivil) {
-		this.estadoCivil = estadoCivil;
-	}
-	public Escolaridade getEscolaridade() {
-		return escolaridade;
-	}
-	public void setEscolaridade(Escolaridade escolaridade) {
-		this.escolaridade = escolaridade;
-	}
 }
